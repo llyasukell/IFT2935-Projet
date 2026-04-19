360b387613042282a7bf4af294f2578333bceed4
Mamour Ndiaye <Mamourndiaye123@gmail.com>
2026-04-19 14:59:43 -0400

    consultation_prix_valides.sql
    qui affiche les produits validés avec leur prix d'estimation et l'expert responsable

---
+++ SQLrequests/consultation_prix_valides.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION;
+
+SELECT U_Annonceur.nom AS nom_annonceur,
+Produit.nom_produit,
+U_Expert.nom AS nom_expert,
+Estimation.prix_estimation
+FROM Annonceur
+JOIN Utilisateur U_Annonceur ON Annonceur.id_utilisateur = U_Annonceur.id
+JOIN Produit ON Annonceur.id_utilisateur = Produit.id_annonceur
+JOIN Estimation ON Estimation.id_produit = Produit.id_produit
+JOIN Expert ON Estimation.id_expert = Expert.id_utilisateur
+JOIN Utilisateur U_Expert ON Expert.id_utilisateur = U_Expert.id
+JOIN Valide ON Valide.id_estimation = Estimation.id_estimation
+    AND Valide.id_annonceur = Produit.id_annonceur
+WHERE Valide.decision = 'valide';
+
+END TRANSACTION;
