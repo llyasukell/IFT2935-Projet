3853136ab2591b521d5f3f5ad424aeec78761f04
Mamour Ndiaye <Mamourndiaye123@gmail.com>
2026-04-19 17:49:11 -0400

    stats_peerformance_expert
    sortir les meilleurs experts : calcule leur total d'estimations validées et combien ils en ont fait

---
+++ SQLrequests/stats_performance_experts.sql
@@ -0,0 +1,14 @@
+BEGIN TRANSACTION
+
+SELECT U_Expert.nom, U_Expert.prenom, Sum(Estimation.prix_estimation) AS total_estime, COUNT(Estimation.id_estimation) AS nb_estimation
+FROM Expert
+JOIN Utilisateur AS U_Expert ON Expert.id_utilisateur = U_Expert.id
+JOIN Estimation ON U_Expert.id_utilisateur = Estimation.id_expert
+JOIN Produit ON Estimation.id_produit = Produit.id_produit
+JOIN Valide ON Estimation.id_estimation = Valide.id_estimation
+WHERE Valide.decision = 'valide'
+GROUP BY U_Expert.nom, U_Expert.prenom
+HAVING COUNT(Estimation.id_estimation) > 1;
+ORDER BY total_estime DESC;
+
+END TRANSACTION
